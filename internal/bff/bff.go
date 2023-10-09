0c2baa87c254a4395252a39b10cbac27c6f3c948
renanbastos93 <renanbastos.tec@gmail.com>
2023-10-09 09:44:26 -0300

    feat: bff starts using weaver Main

---
+++ internal/bff/bff.go
@@ -0,0 +1,32 @@
+package bff
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ServiceWeaver/weaver"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/renanbastos93/transaction-routine/internal/app"
+)
+
+type implBFF struct {
+	weaver.Implements[weaver.Main]
+	app weaver.Ref[app.Component]
+	bff weaver.Listener `weaver:"bff"`
+
+	f *fiber.App
+}
+
+func (e *implBFF) createRouter(ctx context.Context) {
+	router := e.f.Use(logger.New(logger.ConfigDefault))
+	e.RegisterAccounts(router)
+}
+
+func Server(ctx context.Context, e *implBFF) (err error) {
+	fmt.Printf("BFF listener available on %v\n", e.bff)
+
+	e.f = fiber.New()
+	e.createRouter(ctx)
+	return e.f.Listener(e.bff)
+}
