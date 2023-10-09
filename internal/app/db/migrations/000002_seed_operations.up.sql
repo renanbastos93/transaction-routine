4f060f9a7c45a5596c88af9dcf8f4012fd8d6bfd
renanbastos93 <renanbastos.tec@gmail.com>
2023-10-09 09:49:11 -0300

    chore: created migration to add seed operations types

---
+++ internal/app/db/migrations/000002_seed_operations.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO OperationTypes (id, description, is_active) VALUES ("cb93756a-c7d6-4b84-960a-27cb88c37937", "COMPRA A VISTA", 1);
+INSERT INTO OperationTypes (id, description, is_active) VALUES ("57b215b6-ddff-4b0d-8c48-d7535a64ad1b", "COMPRA PARCELADA", 1);
+INSERT INTO OperationTypes (id, description, is_active) VALUES ("5dec0214-4490-422b-ad24-78c413c53aaa", "COMPRA SAQUE", 1);
+INSERT INTO OperationTypes (id, description, is_active) VALUES ("0330bfe8-efc8-4ef8-b8fc-d2462df3a3c1", "PAGAMENTO", 1);
